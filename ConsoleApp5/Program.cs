0b1ebefbd898f004d1497c559c532db81cdbccd7
snct-ukai <73654679+snct-ukai@users.noreply.github.com>
2021-03-01 00:12:22 +0900

    修正

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,11 +1,8 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 using System.Web.NBitcoin;
 
 namespace ConsoleApp5
@@ -14,8 +11,10 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
-            const string api = "****";
+            string api;
 
+            Console.WriteLine("URLを入力してください");
+            api = Console.ReadLine();
             string searchword = Console.ReadLine();
             string SWurl = HttpUtility.UrlEncode(searchword);
             String apiurl = api;
@@ -24,6 +23,7 @@ namespace ConsoleApp5
             StreamReader reader = new StreamReader(Res.GetResponseStream(), new UTF8Encoding(false));
             var obj_from_json = JArray.Parse(reader.ReadToEnd());
 
+            Console.WriteLine("\n");
             Console.WriteLine(obj_from_json);
             Console.ReadLine();
         }
